3c89d9fe53ce64fa8e205bcf870592263dd7005b
Amitoj Singh <amitojsingh366@gmail.com>
2020-09-12 23:42:31 +0300

    third commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import *
+from util import *
+
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/users")
+def users():
+    td = action.readtable("")
+    if not td:
+        td = [('EMPTY', 'EMPTY', 'EMPTY', 'EMPTY')]
+    return render_template("users.html", data=td, dataL=len(td))
+
+
+# @app.route("/test")
+# def test():
+#     return render_template("test.html")
+
+
+@app.route("/insert", methods=["GET", "POST"])
+def post():
+    if request.method == "POST":
+        uname = request.form["username"]
+        pwd = request.form["password"]
+        action.write("", uname, pwd)
+
+        return redirect(url_for("home"))
+    else:
+        return redirect(url_for("home"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ util.py
@@ -0,0 +1,54 @@
+# from flask import *
+
+import mysql.connector
+from mysql.connector import Error
+import random
+
+
+class connect:
+    def start(self):
+        try:
+            connection = mysql.connector.connect(
+                host="localhost", database="demo", user="root", password="test"
+            )
+            if connection.is_connected():
+                db_Info = connection.get_server_info()
+                print("Connected to MySQL Server version ", db_Info)
+                connectioncursor = connection.cursor()
+            else:
+                connection = "fail"
+        except Error as e:
+            print("Error while connecting to MySQL", e)
+            connection = "fail"
+
+        return connection
+
+
+class action:
+    def readtable(self):
+        conn = connect.start("")
+
+        if conn != "fail":
+            conncursor = conn.cursor()
+            a = "SELECT * FROM users;"
+            conncursor.execute(a)
+            exist = conncursor.fetchall()
+            if exist:
+                return exist
+
+    def write(self, uname, pwd):
+        conn = connect.start("")
+        if conn != "fail":
+            conncursor = conn.cursor()
+            ii = 1
+            jj = random.randint(5, 8)
+            uid = ""
+            while ii <= jj:
+                uid = uid + str(random.randint(0, 9))
+                ii += 1
+            a = f"""INSERT INTO `users` (`username`,`password`,`uid`) VALUES ('{uname}','{pwd}','{uid}');"""
+            try:
+                conncursor.execute(a)
+                conn.commit()
+            except Error as ide:
+                print("Error while Saving To MySql", ide)
